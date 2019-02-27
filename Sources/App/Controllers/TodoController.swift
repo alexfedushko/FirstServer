d678ce02c42039bee90b35496e882a987ef0437e
Alex Fediushko <fediushko.o@dbbest.com>
2019-02-27 22:46:23 +0200

    Update

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -1,23 +1,92 @@
 import Vapor
 
+
 /// Controls basic CRUD operations on `Todo`s.
 final class TodoController {
+    
+    init() {
+        
+    }
+    
+    
+    func deleteUsers() {
+        
+    }
     /// Returns a list of all `Todo`s.
-    func index(_ req: Request) throws -> Future<[Todo]> {
-        return Todo.query(on: req).all()
+    func index(_ req: Request) throws -> /*Future<[Todo]>*/Future<CustomResponse> {
+       let obj = CustomResponse()
+       
+let promise = req.eventLoop.newPromise(CustomResponse.self)
+        let arr = Todo.query(on: req).all()
+        
+        obj.message = "All good"
+        //obj.data = arr
+        
+        var tarr = [Todo]()
+        var isDone = false
+        _ = try arr.do({ todo in
+            print("Done")
+            obj.data = todo
+            isDone = true
+            promise.succeed(result: obj)
+        })
+        
+        print("Done2")
+        
+        return promise.futureResult
+        
     }
 
     /// Saves a decoded `Todo` to the database.
     func create(_ req: Request) throws -> Future<Todo> {
-        return try req.content.decode(Todo.self).flatMap { todo in
-            return todo.save(on: req)
+        let obj = try req.content.syncGet(Todo.self, at: "data")
+//        var error =  Abort(.badRequest)
+//        error.reason = "Bad number"
+//        throw error
+
+        guard let title = req.query[String.self, at: "title"] else {
+            throw Abort(.badRequest)
         }
+        
+        guard let customID = req.query[Int.self, at: "id"] else {
+            throw Abort(.badRequest)
+        }
+
+        let par = try req.parameters.next(String.self)
+        let par2 = try req.parameters.next(String.self)
+        
+        let baseObj = obj.save(on: req)
+        
+        return baseObj
     }
 
     /// Deletes a parameterized `Todo`.
-    func delete(_ req: Request) throws -> Future<HTTPStatus> {
-        return try req.parameters.next(Todo.self).flatMap { todo in
-            return todo.delete(on: req)
-        }.transform(to: .ok)
+    func delete(_ req: Request) throws -> Future<String> {
+        guard let customID = req.query[Int.self, at: "id"] else {
+            throw Abort(.badRequest)
+        }
+        
+        return Todo.find(customID, on: req).flatMap({ todo -> Future<Void> in
+            guard let temp = todo else {
+                var error = Abort(.notFound)
+                error.reason = "Object with id \(customID) not found"
+                throw error
+            }
+
+            return temp.delete(on: req)
+        }).transform(to: "Delete object with id \(customID) succes")
+        
+//        return isDelete ? "Delete object with id \(customID) succes" : "object not found"
+//
+//
+//        return "buuuuu"
+        //        syncConc.sync {
+        //
+        //        }
+        
+        //        obj.de
+//        return try req.parameters.next(Todo.self).flatMap { todo in
+//            return todo.delete(on: req)
+//        }.transform(to: .ok)
     }
 }
--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -1,6 +1,27 @@
 import Vapor
 
-/// Called after your application has initialized.
+
+
+func foo(on container: Container) {
+    let future = container.withPooledConnection(to: .sqlite) { (conn) -> EventLoopFuture<Todo> in
+        let a = try Todo.query(on: conn).filter(\.id, ._greaterThanOrEqual, 5).all().do({ todo in
+            for obj in todo {
+                obj.delete(on: conn)
+            }
+        })
+        return  Todo.query(on: conn).first().unwrap(or: Abort(.badRequest))
+    }
+//    future.do{ msg in
+//        print(msg)
+//        }.catch{ error in
+//            print("\(error.localizedDescription)")
+//    }
+}
+
 public func boot(_ app: Application) throws {
-    // Your code here
+    func runRepeatTimer() {
+        app.eventLoop.scheduleTask(in: TimeAmount.seconds(10), runRepeatTimer)
+        foo(on: app)
+    }
+    runRepeatTimer()
 }
